d165438e6105e0f9f7ae21c28e9cada2cb0ed60e
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-05-02 11:30:48 +0200

    New structure

---
+++ _header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<!-- Mirrored from preview.colorlib.com/theme/coffee/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 02 May 2021 08:36:57 GMT -->
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link rel="shortcut icon" href="img/fav.html">
+
+<meta name="author" content="Jean-pierre14">
+
+<meta name="description" content="">
+
+<meta name="keywords" content="">
+
+<meta charset="UTF-8">
+
+<title>Coffee</title>
+<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+
+<link rel="stylesheet" href="css/linearicons.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/magnific-popup.css">
+<link rel="stylesheet" href="css/nice-select.css">
+<link rel="stylesheet" href="css/animate.min.css">
+<link rel="stylesheet" href="css/owl.carousel.css">
+<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <?php include './config/config.jp.php';?>
+    <header id="header" id="home">
+  <div class="header-top">
+    <div class="container">
+      <div class="row justify-content-end">
+        <div class="col-lg-8 col-sm-4 col-8 header-top-right no-padding">
+          <ul>
+            <li>
+              Mon-Fri: 8am to 2pm
+            </li>
+            <li>
+              Sat-Sun: 11am to 4pm
+            </li>
+            <li>
+              <a href="<?= $numero;?>"><?= $numero;?></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row align-items-center justify-content-between d-flex">
+      <div id="logo">
+        <a href="index-2.html"><img src="img/logo.png" alt="" title="" /></a>
+      </div>
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#coffee">Coffee</a></li>
+          <li><a href="#review">Review</a></li>
+          <li><a href="#blog">Blog</a></li>
+          <li class="menu-has-children"><a href="#">Pages</a>
+            <ul>
+              <li><a href="generic.html">Generic</a></li>
+              <li><a href="elements.html">Elements</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</header>
---
+++ index.php
@@ -0,0 +1,424 @@
+<?php include '_header.php';?>
+
+
+<section class="banner-area" id="home">
+  <div class="container">
+    <div class="row fullscreen d-flex align-items-center justify-content-start">
+      <div class="banner-content col-lg-7">
+        <h6 class="text-white text-uppercase">Now you can feel the Energy</h6>
+        <h1>
+          Start your day with <br>
+          a black Coffee
+        </h1>
+        <a href="#" class="primary-btn text-uppercase">Buy Now</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="video-sec-area pb-100 pt-40" id="about">
+  <div class="container">
+    <div class="row justify-content-start align-items-center">
+      <div class="col-lg-6 video-right justify-content-center align-items-center d-flex">
+        <div class="overlay overlay-bg"></div>
+        <a class="play-btn" href="https://www.youtube.com/watch?v=ARA0AxrnHdM"><img class="img-fluid"
+            src="img/play-icon.png" alt=""></a>
+      </div>
+      <div class="col-lg-6 video-left">
+        <h6>Live Coffee making process.</h6>
+        <h1>We Telecast our <br>
+          Coffee Making Live</h1>
+        <p><span>We are here to listen from you deliver exellence</span></p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp or incididunt ut labore et
+          dolore magna aliqua. Ut enim ad minim.
+        </p>
+        <img class="img-fluid" src="img/signature.png" alt="">
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="menu-area section-gap" id="coffee">
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="menu-content pb-60 col-lg-10">
+        <div class="title text-center">
+          <h1 class="mb-10">What kind of Coffee we serve for you</h1>
+          <p>Who are in extremely love with eco friendly system.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="single-menu">
+          <div class="title-div justify-content-between d-flex">
+            <h4>Cappuccino</h4>
+            <p class="price float-right">
+              $49
+            </p>
+          </div>
+          <p>
+            Usage of the Internet is becoming more common due to rapid advance.
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="single-menu">
+          <div class="title-div justify-content-between d-flex">
+            <h4>Americano</h4>
+            <p class="price float-right">
+              $49
+            </p>
+          </div>
+          <p>
+            Usage of the Internet is becoming more common due to rapid advance.
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="single-menu">
+          <div class="title-div justify-content-between d-flex">
+            <h4>Espresso</h4>
+            <p class="price float-right">
+              $49
+            </p>
+          </div>
+          <p>
+            Usage of the Internet is becoming more common due to rapid advance.
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="single-menu">
+          <div class="title-div justify-content-between d-flex">
+            <h4>Macchiato</h4>
+            <p class="price float-right">
+              $49
+            </p>
+          </div>
+          <p>
+            Usage of the Internet is becoming more common due to rapid advance.
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="single-menu">
+          <div class="title-div justify-content-between d-flex">
+            <h4>Mocha</h4>
+            <p class="price float-right">
+              $49
+            </p>
+          </div>
+          <p>
+            Usage of the Internet is becoming more common due to rapid advance.
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="single-menu">
+          <div class="title-div justify-content-between d-flex">
+            <h4>Coffee Latte</h4>
+            <p class="price float-right">
+              $49
+            </p>
+          </div>
+          <p>
+            Usage of the Internet is becoming more common due to rapid advance.
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="single-menu">
+          <div class="title-div justify-content-between d-flex">
+            <h4>Piccolo Latte</h4>
+            <p class="price float-right">
+              $49
+            </p>
+          </div>
+          <p>
+            Usage of the Internet is becoming more common due to rapid advance.
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="single-menu">
+          <div class="title-div justify-content-between d-flex">
+            <h4>Ristretto</h4>
+            <p class="price float-right">
+              $49
+            </p>
+          </div>
+          <p>
+            Usage of the Internet is becoming more common due to rapid advance.
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="single-menu">
+          <div class="title-div justify-content-between d-flex">
+            <h4>Affogato</h4>
+            <p class="price float-right">
+              $49
+            </p>
+          </div>
+          <p>
+            Usage of the Internet is becoming more common due to rapid advance.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="gallery-area section-gap" id="gallery">
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="menu-content pb-60 col-lg-10">
+        <div class="title text-center">
+          <h1 class="mb-10">What kind of Coffee we serve for you</h1>
+          <p>Who are in extremely love with eco friendly system.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4">
+        <a href="img/g1.jpg" class="img-pop-home">
+          <img class="img-fluid" src="img/g1.jpg" alt="">
+        </a>
+        <a href="img/g2.jpg" class="img-pop-home">
+          <img class="img-fluid" src="img/g2.jpg" alt="">
+        </a>
+      </div>
+      <div class="col-lg-8">
+        <a href="img/g3.jpg" class="img-pop-home">
+          <img class="img-fluid" src="img/g3.jpg" alt="">
+        </a>
+        <div class="row">
+          <div class="col-lg-6">
+            <a href="img/g4.jpg" class="img-pop-home">
+              <img class="img-fluid" src="img/g4.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a href="img/g5.jpg" class="img-pop-home">
+              <img class="img-fluid" src="img/g5.jpg" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="review-area section-gap" id="review">
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="menu-content pb-60 col-lg-10">
+        <div class="title text-center">
+          <h1 class="mb-10">What kind of Coffee we serve for you</h1>
+          <p>Who are in extremely love with eco friendly system.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-6 col-md-6 single-review">
+        <img src="img/r1.png" alt="">
+        <div class="title d-flex flex-row">
+          <h4>lorem ipusm</h4>
+          <div class="star">
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+          </div>
+        </div>
+        <p>
+          Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
+          Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
+        </p>
+      </div>
+      <div class="col-lg-6 col-md-6 single-review">
+        <img src="img/r2.png" alt="">
+        <div class="title d-flex flex-row">
+          <h4>lorem ipusm</h4>
+          <div class="star">
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+          </div>
+        </div>
+        <p>
+          Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
+          Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
+        </p>
+      </div>
+    </div>
+    <div class="row counter-row">
+      <div class="col-lg-3 col-md-6 single-counter">
+        <h1 class="counter">2536</h1>
+        <p>Happy Client</p>
+      </div>
+      <div class="col-lg-3 col-md-6 single-counter">
+        <h1 class="counter">7562</h1>
+        <p>Total Projects</p>
+      </div>
+      <div class="col-lg-3 col-md-6 single-counter">
+        <h1 class="counter">2013</h1>
+        <p>Cups Coffee</p>
+      </div>
+      <div class="col-lg-3 col-md-6 single-counter">
+        <h1 class="counter">10536</h1>
+        <p>Total Submitted</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="blog-area section-gap" id="blog">
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="menu-content pb-60 col-lg-10">
+        <div class="title text-center">
+          <h1 class="mb-10">What kind of Coffee we serve for you</h1>
+          <p>Who are in extremely love with eco friendly system.</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-6 col-md-6 single-blog">
+        <img class="img-fluid" src="img/b1.jpg" alt="">
+        <ul class="post-tags">
+          <li><a href="#">Travel</a></li>
+          <li><a href="#">Life Style</a></li>
+        </ul>
+        <a href="#">
+          <h4>Portable latest Fashion for young women</h4>
+        </a>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore.
+        </p>
+        <p class="post-date">
+          31st January, 2018
+        </p>
+      </div>
+      <div class="col-lg-6 col-md-6 single-blog">
+        <img class="img-fluid" src="img/b2.jpg" alt="">
+        <ul class="post-tags">
+          <li><a href="#">Travel</a></li>
+          <li><a href="#">Life Style</a></li>
+        </ul>
+        <a href="#">
+          <h4>Portable latest Fashion for young women</h4>
+        </a>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore.
+        </p>
+        <p class="post-date">
+          31st January, 2018
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<footer class="footer-area section-gap">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-5 col-md-6 col-sm-6">
+        <div class="single-footer-widget">
+          <h6>About Us</h6>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore
+            magna aliqua.
+          </p>
+          <p class="footer-text">
+
+            Copyright &copy;
+            <script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with
+            <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com/"
+              target="_blank">Colorlib</a>
+
+          </p>
+        </div>
+      </div>
+      <div class="col-lg-5  col-md-6 col-sm-6">
+        <div class="single-footer-widget">
+          <h6>Newsletter</h6>
+          <p>Stay update with our latest</p>
+          <div class="" id="mc_embed_signup">
+            <form target="_blank" novalidate="true"
+              action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+              method="get" class="form-inline">
+              <input class="form-control" name="EMAIL" placeholder="Enter Email" onfocus="this.placeholder = ''"
+                onblur="this.placeholder = 'Enter Email '" required="" type="email">
+              <button class="click-btn btn btn-default"><i class="fa fa-long-arrow-right"
+                  aria-hidden="true"></i></button>
+              <div style="position: absolute; left: -5000px;">
+                <input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
+              </div>
+              <div class="info pt-20"></div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-2 col-md-6 col-sm-6 social-widget">
+        <div class="single-footer-widget">
+          <h6>Follow Us</h6>
+          <p>Let us be social</p>
+          <div class="footer-social d-flex align-items-center">
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-dribbble"></i></a>
+            <a href="#"><i class="fa fa-behance"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<script src="js/vendor/jquery-2.2.4.min.js"></script>
+<script src="../../../cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+  integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="js/vendor/bootstrap.min.js"></script>
+<script type="text/javascript"
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+<script src="js/easing.min.js"></script>
+<script src="js/hoverIntent.js"></script>
+<script src="js/superfish.min.js"></script>
+<script src="js/jquery.ajaxchimp.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.sticky.js"></script>
+<script src="js/jquery.nice-select.min.js"></script>
+<script src="js/parallax.min.js"></script>
+<script src="js/waypoints.min.js"></script>
+<script src="js/jquery.counterup.min.js"></script>
+<script src="js/mail-script.js"></script>
+<script src="js/main.js"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+</body>
+
+<!-- Mirrored from preview.colorlib.com/theme/coffee/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 02 May 2021 08:37:18 GMT -->
+
+</html>
