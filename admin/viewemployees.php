f6bab31639704c79dc3429ced767fd086f6d68f1
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-05-29 19:37:09 +0200

    Employees oper event

--- admin/viewemployees.php
+++ admin/viewemployees.php
@@ -173,7 +173,25 @@
                                         </div>
 
                                         <div class="col-md-5">
-                                            <div class="card">
+                                            <div class="card mb-1">
+                                                <div class="card-body">
+                                                    <form action="" method="post">
+                                                        <div class="form-group">
+                                                            <label for="oper">Oper</label> <span
+                                                                class="badge badge-success"><?= $row['oper']; ?></span>
+                                                            <select name="oper" id="oper" class="form-control">
+                                                                <option value="">-- select --</option>
+                                                                <option value="OPERATIONNEL">Operationnel</option>
+                                                                <option value="RETIRED">Retired</option>
+                                                                <option value="REMOVED">Removed</option>
+                                                            </select>
+                                                            <button class="my-1 btn btn-success"
+                                                                type="button">Record</button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                            <div class="card my-1">
                                                 <div class="card-body">
                                                     <form action="" method="post">
                                                         <div class="form-group">
