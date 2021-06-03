88d0f0b51af959671b1666c5bc07189b5e57ef59
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-06-03 22:56:09 +0200

    username in title

--- users/index.php
+++ users/index.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['username'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <title></title>
+    <title><?= $_SESSION['username']; ?></title>
 </head>
 
 <body>
