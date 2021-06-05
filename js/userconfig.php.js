0ce6e0938f1743085f4d5079b4d8469deebbc4b3
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-06-05 10:18:54 +0200

    User UI with data from SQL using dataType Json

--- js/userconfig.php.js
+++ js/userconfig.php.js
@@ -35,17 +35,64 @@ function userProfil() {
                                         <span>${data.fullname}</span> 
                                     </h5>
                                 </div>
-                                <div class="col-md-6"></div>
+                                <div class="col-md-9">
+                                    <h5 class="d-flex justify-content-between align-content-center">
+                                        <span>Email:</span>
+                                        <span>${data.email}</span> 
+                                    </h5>
+                                </div>
+                                <div class="col-md-4">
+                                    <p class="d-flex justify-content-between align-content-center">
+                                        <span>Gender: </span>
+                                        <span class="badge badge-success">${data.gender}</span>
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p class="d-flex justify-content-between align-content-center">
+                                        <span>Partner: </span>
+                                        <span class="badge badge-success">${data.woman}</span>
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p class="d-flex justify-content-between align-content-center">
+                                        <span>Number of children: </span>
+                                        <span class="badge badge-success">${data.children}</span>
+                                    </p>
+                                </div>
+                                <div class="col-md-4">
+                                    <p class="d-flex justify-content-between align-content-center">
+                                        <span>Salary: </span>
+                                        <span class="badge badge-success">${data.salary}, 00$</span>
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div class="card-footer">
                         <button type="button" class="btn btn-success">Print</button>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">I have a request</button>
+                    </div>
+                </div>
+                <div class="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">My request</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                        </div>
                     </div>
                 </div>
                 `)
             }
         })
     }
-    setTimeout('userProfil()', 1000)
+    // setTimeout('userProfil()', 1000)
 }
