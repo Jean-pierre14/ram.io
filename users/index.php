2004a5b6b8802bd47ffb23728b41f5fbb83958b7
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-06-07 13:31:56 +0200

    PrintThis config

--- users/index.php
+++ users/index.php
@@ -91,28 +91,32 @@ if (!isset($_SESSION['username'])) {
 <script src="../js/userconfig.php.js"></script>
 <script>
 jQuery(document).ready(function() {
-    $("#printThis").printThis({
-        debug: false, // show the iframe for debugging
-        importCSS: true, // import parent page css
-        importStyle: false, // import style tags
-        printContainer: true, // print outer container/$.selector
-        loadCSS: "", // path to additional css file - use an array [] for multiple
-        pageTitle: "", // add title to print page
-        removeInline: false, // remove inline styles from print elements
-        removeInlineSelector: "*", // custom selectors to filter inline styles. removeInline must be true
-        printDelay: 333, // variable print delay
-        header: `<h2><?= $_SESSION['fullname']; ?></h2>`, // prefix to html
-        footer: null, // postfix to html
-        base: false, // preserve the BASE tag or accept a string for the URL
-        formValues: true, // preserve input/form values
-        canvas: false, // copy canvas content
-        doctypeString: '...', // enter a different doctype for older markup
-        removeScripts: false, // remove script tags from print content
-        copyTagClasses: false, // copy classes from the html & body tag
-        beforePrintEvent: null, // function for printEvent in iframe
-        beforePrint: null, // function called before iframe is filled
-        afterPrint: null // function called before iframe is removed
-    });
+
+    $(document).on('click', '#printThis', function() {
+
+        $("#mySelector").printThis({
+            debug: false, // show the iframe for debugging
+            importCSS: true, // import parent page css
+            importStyle: false, // import style tags
+            printContainer: true, // print outer container/$.selector
+            loadCSS: "", // path to additional css file - use an array [] for multiple
+            pageTitle: "", // add title to print page
+            removeInline: false, // remove inline styles from print elements
+            removeInlineSelector: "*", // custom selectors to filter inline styles. removeInline must be true
+            printDelay: 333, // variable print delay
+            header: null, // prefix to html
+            footer: null, // postfix to html
+            base: false, // preserve the BASE tag or accept a string for the URL
+            formValues: true, // preserve input/form values
+            canvas: false, // copy canvas content
+            doctypeString: '...', // enter a different doctype for older markup
+            removeScripts: false, // remove script tags from print content
+            copyTagClasses: false, // copy classes from the html & body tag
+            beforePrintEvent: null, // function for printEvent in iframe
+            beforePrint: null, // function called before iframe is filled
+            afterPrint: null // function called before iframe is removed
+        });
+    })
     $(document).on('click', '#SendRequest', function() {
 
         let email = $('#user_id').val()
