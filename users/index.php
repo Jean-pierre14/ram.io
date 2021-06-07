e7ebec7f8c2e4d66d74fb868eca6c3778332180a
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-06-07 06:27:36 +0200

    PrintThis

--- users/index.php
+++ users/index.php
@@ -89,6 +89,28 @@ if (!isset($_SESSION['username'])) {
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script src="../js/userconfig.php.js"></script>
 <script>
+$("#mySelector").printThis({
+    debug: false, // show the iframe for debugging
+    importCSS: true, // import parent page css
+    importStyle: false, // import style tags
+    printContainer: true, // print outer container/$.selector
+    loadCSS: "", // path to additional css file - use an array [] for multiple
+    pageTitle: "", // add title to print page
+    removeInline: false, // remove inline styles from print elements
+    removeInlineSelector: "*", // custom selectors to filter inline styles. removeInline must be true
+    printDelay: 333, // variable print delay
+    header: null, // prefix to html
+    footer: null, // postfix to html
+    base: false, // preserve the BASE tag or accept a string for the URL
+    formValues: true, // preserve input/form values
+    canvas: false, // copy canvas content
+    doctypeString: '...', // enter a different doctype for older markup
+    removeScripts: false, // remove script tags from print content
+    copyTagClasses: false, // copy classes from the html & body tag
+    beforePrintEvent: null, // function for printEvent in iframe
+    beforePrint: null, // function called before iframe is filled
+    afterPrint: null // function called before iframe is removed
+});
 jQuery(document).ready(function() {
 
     $(document).on('click', '#SendRequest', function() {
