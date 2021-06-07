cf20fdf90d0d605347b9f06eb2eb21494270a1f2
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-06-07 13:12:07 +0200

    Order into the user UI

--- users/index.php
+++ users/index.php
@@ -24,25 +24,26 @@ if (!isset($_SESSION['username'])) {
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+        <a class="navbar-brand" href="index.php"><?= $prooject_name; ?>/<?= $_SESSION['username']; ?></a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.php">Profile</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="../logout.php">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <div class="container-fluid">
-        <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-            <a class="navbar-brand" href="index.php"><?= $prooject_name; ?>/<?= $_SESSION['username']; ?></a>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="index.php">Profile</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="../logout.php">Logout</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
         <div class="container">
             <div class="row justify-content-center">
                 <input type="hidden" value="<?= $_SESSION['id']; ?>" id="myprofiledata">
