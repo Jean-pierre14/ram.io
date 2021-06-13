6e9b953328bec44294c0742037df19d9c113902e
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-06-13 13:58:14 +0200

    Dashboard part 2

--- admin/index.php
+++ admin/index.php
@@ -395,33 +395,7 @@
                                                         <td>2011/04/25</td>
                                                         <td>$320,800</td>
                                                     </tr>
-                                                    <tr>
-                                                        <td>Tiger Nixon</td>
-                                                        <td>System Architect</td>
-                                                        <td>Edinburgh</td>
-                                                        <td>61</td>
-                                                        <td>2011/04/25</td>
-                                                        <td>$320,800</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Tiger Nixon</td>
-                                                        <td>System Architect</td>
-                                                        <td>Edinburgh</td>
-                                                        <td>61</td>
-                                                        <td>2011/04/25</td>
-                                                        <td>$320,800</td>
-                                                    </tr>
-                                                </tbody>
-                                                <tfoot>
-                                                    <tr>
-                                                        <th>Name</th>
-                                                        <th>Position</th>
-                                                        <th>Office</th>
-                                                        <th>Age</th>
-                                                        <th>Start date</th>
-                                                        <th>Salary</th>
-                                                    </tr>
-                                                </tfoot>
+                                                    </tfoot>
                                             </table>
                                         </div>
                                     </div>
