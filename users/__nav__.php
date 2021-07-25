e32d51b5b29347a47672c0b43244ae189a70935c
User <chiruzabisimwa@gmail.com>
2021-07-25 15:47:19 +0200

    Stream part Three and the end of it Done

--- users/__nav__.php
+++ users/__nav__.php
@@ -5,6 +5,27 @@
         public $context;
         // This is to store the URLs
         private $url;
+
+        public function stream_opean(string $path, string $mode, int $option, string &$openedpath) : bool {
+            $url = parser_url($path);
+            if($url === false) return false;
+            $this->url = $url['host'] . '/' . $url['path'];
+            return true; 
+        }
+        // to heandler to fwrite() to this stream
+        public function stream_write(string $data) : int {
+            $this->buffer = $data;
+            return strlen($data);
+        }
+        public function stream_close(){
+            $curl = curl_init("http://" . $this->url);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $this->buffer);
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PATCH");
+            curl_exec($curl);
+            curl_close($curl);
+            $this->buffer = "";
+        }
+        
     }
 ?>
 <nav class="navbar navbar-expand-md bg-dark navbar-dark">
