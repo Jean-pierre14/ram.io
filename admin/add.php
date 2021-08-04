3628c63593d147aec1ce8557221ded74df20b838
User <chiruzabisimwa@gmail.com>
2021-08-04 14:14:26 +0200

    synthax

--- admin/add.php
+++ admin/add.php
@@ -4,6 +4,7 @@
     <link rel="stylesheet" href="./assets/Addstyle.css">
 </head>
 <body>
-    <a href="addemployee" class="btn add-btn"><i class="fa fa-add-user"></i> Add Employee</a>
+    <a href="addemployee" class="btn add-btn">
+        <i class="fa fa-add-user"></i> Add Employee</a>
 </body>
 </html>
