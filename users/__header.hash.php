0c5be89e7212e03a92ed66c997d879ac7acaa9c6
User <chiruzabisimwa@gmail.com>
2021-08-20 22:04:56 +0200

    Setup design done

--- users/__header.hash.php
+++ users/__header.hash.php
@@ -1,80 +1,19 @@
-<?php include '../config/config.jp.php';
-session_start();
-
-if (!isset($_SESSION['username'])) {
-    header("Location: ../login.php");
-}else{
-    $username = $_SESSION['username'];
-    $id = $_SESSION['id'];
-    $sql = mysqli_query($con, "SELECT * FROM employees_tb WHERE username = '".$_SESSION['username']."' AND id = $id");
-    if(mysqli_num_rows($sql) == 1){
-        // return true;
-    }else{
-        header("Location: ../login.php");
-    }
-}
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="Content-Language" content="en">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
-    <meta name="description" content="Jean-pierre14 and Titans">
-    <link rel="stylesheet" href="../admin/main.07a59de7b920cd76b874.css">
-    <link rel="shortcut icon" href="../admin/assets/images/ramallLogo.png" type="image/x-icon">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <title>Ramail.org
-        <?= $_SESSION['username']; ?>
-    </title>
-    <style>
-        * {
-            box-sizing: border-box
-        }
-        ::-webkit-scrollbar{
-            width: 5px;
-        }
-        ::-webkit-scrollbar-track{
-            background-color: #ccc;
-        }
-        ::-webkit-scrollbar-thumb{
-            background-color: coral;
-        }
-        #ListUser{
-            position: relative;
-            width: 100%;
-            height: calc(100vh - 100px);
-            overflow-y: auto;
-        }
-        #messageOfThisUser{
-            position: relative;
-            width: 100%;
-            height: 400px;
-        }
-        #profileContainer,
-        #messageContainer,
-        #salaryContainer
-        {
-            width: 100%;
-            max-height: calc(100vh - 70px);
-            overflow-y:auto;
-        }
-
-        .msgBox {
-            position: relative;
-            padding: 10px;
-            width: 70%;
-            display: block;
-            margin: 5px 0;
-            border-radius: 0.5rem;
-            background: rgb(248, 253, 179);
-        }
-    </style>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <title>
+    RamaLL
+  </title>
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/blk-design-system.css?v=1.0.0" rel="stylesheet" />
+  <link href="../assets/demo/demo.css" rel="stylesheet" />
 </head>
 
-<body>
+<body class="profile-page">
--- users/__nav__.php
+++ users/__nav__.php
@@ -1,27 +1,55 @@
-
-<nav class="navbar navbar-expand-md navbar-fixed bg-dark navbar-dark">
-    <a class="navbar-brand" href="index.php">
-        <?= $prooject_name; ?> /
-        <?= $_SESSION['username']; ?>
-    </a>
-
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link active" id="profileContext" href="javascript:void(0)">Profile</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="messageContext" href="javascript:void(0)">messages</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="salaryContext" href="javascript:void(0)">Salary</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="../logout.php">Logout</a>
-            </li>
+<nav class="navbar navbar-expand-lg fixed-top navbar-transparent " color-on-scroll="100">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="index.php" rel="tooltip" title="Designed and Coded by Creative Tim"
+          data-placement="bottom" target="_blank">
+          <span>RamaLL•</span> Employee System
+        </a>
+        <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
+          aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-bar bar1"></span>
+          <span class="navbar-toggler-bar bar2"></span>
+          <span class="navbar-toggler-bar bar3"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse justify-content-end" id="navigation">
+        <div class="navbar-collapse-header">
+          <div class="row">
+            <div class="col-6 collapse-brand">
+              <a>
+                RamaLL•
+              </a>
+            </div>
+            <div class="col-6 collapse-close text-right">
+              <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navigation"
+                aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="tim-icons icon-simple-remove"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <ul class="nav nav-tabs nav-tabs-primary justify-content-center">
+          <li class="nav-item">
+            <a class="nav-link active" data-toggle="tab" href="#linka">
+              Profil
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#linkb">
+              Salary
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#linkc">
+              News
+            </a>
+          </li>
         </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="../logout.php">Logout</a>
+          </li>
+        </ul>
+      </div>
     </div>
-</nav>
+  </nav>
