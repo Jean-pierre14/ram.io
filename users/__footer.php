c4d4f7744be413a767ddcdfde359713239563277
User <chiruzabisimwa@gmail.com>
2021-08-20 22:37:13 +0200

    Get json data

--- users/__footer.php
+++ users/__footer.php
@@ -1,13 +1,15 @@
 </body>
-    <script src="../assets/js/core/jquery.min.js" type="text/javascript"></script>
-    <script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
-    <script src="../assets/js/core/bootstrap.min.js" type="text/javascript"></script>
-    <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-    <script src="../assets/js/plugins/bootstrap-switch.js"></script>
-    <script src="../assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
-    <script src="../assets/js/plugins/chartjs.min.js"></script>
-    <script src="../assets/js/plugins/moment.min.js"></script>
-    <script src="../assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
-    <script src="../assets/demo/demo.js"></script>
-    <script src="../assets/js/blk-design-system.min.js?v=1.0.0" type="text/javascript"></script>
+<script src="../assets/js/core/jquery.min.js" type="text/javascript"></script>
+<script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="../assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+<script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+<script src="../assets/js/plugins/bootstrap-switch.js"></script>
+<script src="../assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+<script src="../assets/js/plugins/chartjs.min.js"></script>
+<script src="../assets/js/plugins/moment.min.js"></script>
+<script src="../assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+<script src="../assets/demo/demo.js"></script>
+<script src="../assets/js/blk-design-system.min.js?v=1.0.0" type="text/javascript"></script>
+<script src="../js/UserConfig.min.js"></script>
+
 </html>
