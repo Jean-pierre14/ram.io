99f59a58a2e6802333e737423ab10c054d20b441
User <chiruzabisimwa@gmail.com>
2021-08-29 19:15:48 +0200

    Favicon fix

--- users/__header.hash.php
+++ users/__header.hash.php
@@ -11,8 +11,8 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+    <meta name="description" content="Ghost and Titans">
+    <link rel="shortcut icon" href="./assets/images/ramallLogo.png" type="image/x-icon">
   <title>
     RamaLL
   </title>
