255b0cc40da3b7b7f2ff0f3a3c1b0843b8a758f8
User <chiruzabisimwa@gmail.com>
2021-08-29 19:11:50 +0200

    Endpoint fix

--- users/index.php
+++ users/index.php
@@ -156,7 +156,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="./" class="nav-link">
                   Profile
                 </a>
               </li>
